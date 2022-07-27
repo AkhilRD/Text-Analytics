e638a8dec3ac3a3de347a00ad78d67777cece788
AkhilRD <105431583+AkhilRD@users.noreply.github.com>
2022-07-28 00:02:07 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -11,6 +11,7 @@ import numpy as np
 import nltk
 nltk.download('stopwords')
 nltk.download('wordnet')
+nltk.download('omw-1.4')
 import unicodedata
 from collections import Counter
 from nltk.stem import WordNetLemmatizer
